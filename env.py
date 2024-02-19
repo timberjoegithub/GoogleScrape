e0121303453e2cd1f92cc7f7063a30f936dafd3f
Joe Steele <jsteele@mathworks.com>
2024-02-19 18:06:11 -0500

    rewrite

--- env.py
+++ env.py
@@ -1,2 +1,7 @@
-URL = "https://www.google.com/maps/place/Google+UK/@51.5332608,-0.1304879,17z/data=!4m7!3m6!1s0x48761b3c54efa6e1:0xc7053ab04745950d!8m2!3d51.5332609!4d-0.1260032!9m1!1b1"
-DriverLocation = "./Driver/chromedriver.exe"
+URL = "https://www.google.com/maps/contrib/109274792898041753066/reviews/"
+
+import platform
+if platform.system() eq 'Linux'
+    DriverLocation = "./Driver/chromedriver"
+else:
+    DriverLocation = "./Driver/chromedriver.exe"
